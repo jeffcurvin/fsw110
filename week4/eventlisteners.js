38bda0013095ba213be2d89dab97132a9b1f0554
jeffcurvin <94943853+jeffcurvin@users.noreply.github.com>
2022-02-17 18:01:43 -0500

    work in progress

---
+++ week4/eventlisteners.js
@@ -0,0 +1,52 @@
+const square = document.getElementById('square')
+
+square.addEventListener('click', function(){
+
+})
+
+square.addEventListener('mouseover',function(){
+  square.style.backgroundColor="green"
+})
+//square.addEventListener('load',function(){
+  //  square.style.backgroundColor = "black"
+//})
+
+
+square.addEventListener('mousedown',function(){
+    square.style.backgroundColor="yellow"
+  })
+
+  square.addEventListener('mouseup',function(){
+    square.style.backgroundColor="blue"
+  })
+
+  square.addEventListener('dblclick',function(){
+    square.style.backgroundColor="red"
+  })
+
+  square.addEventListener('wheel',function(){
+    square.style.backgroundColor="purple"
+  })
+
+  document.addEventListener('keydown',function(event){
+    switch(event.code){
+        case'KeyR':
+            square.style.backgroundColor="red"
+            break;
+            case'KeyP':
+            square.style.backgroundColor="purple"
+            break;
+            case'KeyG':
+            square.style.backgroundColor="green"
+            break;
+            case'KeyB':
+            square.style.backgroundColor="black"
+            break;
+            case'KeyY':
+            square.style.backgroundColor="yellow"
+            break;
+  }})
+square.addEventListener('',function(){
+    square.style.backgroundColor = "black"
+})
+  
